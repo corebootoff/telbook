594e4715128593aa179aef8363c815d90c66d7a6
Anton <aceanton88@gmail.com>
2019-04-01 06:24:49 +0500

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,10 +1,17 @@
 import shutil
 import sys
 import models
-from constant import *
 
-main = models.Menu(None, MAINMENU)
+main = models.Menu('Главное меню', None)
 
-telmenu = models.Menu(main, TELMENU)
+tel_menu = models.Menu('Телефонная книга', main)
+show_all = models.Command('Посмотреть всю книгу', tel_menu)
+add_contact = models.Command('Добавить контакт', tel_menu)
+remove_contact = models.Command('Удалить контакт', tel_menu)
+exit_tel_menu = models.Command('Назад', tel_menu)
 
-main.show_menu()
+site_menu = models.Menu('Книга сайтов', main)
+preferences = models.Menu('Настройки', main)
+exit_main = models.Command('Выход', main)
+
+main.choose_command()
