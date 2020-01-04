ca9b3506e4855857a2a99f4d451662832f3a06d4
Korbutov Anton <aceanton88@gmail.com>
2020-01-04 17:58:33 +0500

    Add all files from notebook

--- gui.py
+++ gui.py
@@ -1,17 +1,10 @@
 import shutil
 import sys
 import models
+from constant import *
 
-main = models.Menu('Главное меню', None)
+main = models.Menu(None, MAINMENU)
 
-tel_menu = models.Menu('Телефонная книга', main)
-show_all = models.Command('Посмотреть всю книгу', tel_menu)
-add_contact = models.Command('Добавить контакт', tel_menu)
-remove_contact = models.Command('Удалить контакт', tel_menu)
-exit_tel_menu = models.Command('Назад', tel_menu)
+telmenu = models.Menu(main, TELMENU)
 
-site_menu = models.Menu('Книга сайтов', main)
-preferences = models.Menu('Настройки', main)
-exit_main = models.Command('Выход', main)
-
-main.choose_command()
+main.show_menu()
--- models.py
+++ models.py
@@ -3,60 +3,38 @@ import sys
 
 
 class Menu(object):
-    '''
-        name - название папки в списке для отображения
-        parent - родительская папка, если None, то создается главное меню
-        welcome - надпись в рамке 40
-        command_list - список объектов вложенных папок
-    '''
-    def __init__(self, name, parent, welcome='Добро пожаловать в адресную книгу ntone!'):
-        self.name = name
+    """
+       welcome      название заголовка. фиксированная длина 40 символов
+       parent       имя модуля родителя
+       show_list    меню для отображения
+       command_list содержит словарь с номером и соответствующей функцией
+    """
+    terminal_window = shutil.get_terminal_size((80,20)) 
+    SEPARATOR = '=' * terminal_window.columns
+    WELCOME = '\n' * terminal_window.lines + int(terminal_window.columns / 2 - 20) * '=' + {namemenu} + int(terminal_window.columns / 2 - 20) * '='
+    def __init__(self, parent, show_list, command_list, welcome=NAMEMENU['main']):
         self.parent = parent
-        if parent:
-            self.parent.show_list.append(self.name)
-            self.parent.command_list.append(self)
-        self.show_list = []
-        self.command_list = [] 
-        self.terminal_window = shutil.get_terminal_size((80,20))     
-        self.welcome = (int(self.terminal_window.columns / 2 - 20) * '=' + '{namemenu}' + int(self.terminal_window.columns / 2 - 20) * '='+'\n' * (self.terminal_window.lines-len(self.command_list)-7)).format(namemenu = welcome)
+        self.show_list = show_list
+        self.welcome = welcome
+        self.command_list = command_list
 
     def show_menu(self):
-        """Отображение меню"""
-        print(self.welcome)      
-        for i in range(1,len(self.show_list)+1):
-            print('{n}. {command}'.format(n = i, command = self.show_list[i - 1]))
+        
+        print(self.SEPARATOR)
+        print(self.WELCOME.format(namemenu = self.welcome))
+        for i in range(1,len(self.command_list)+1):
+            print('{n}. {command}'.format(n = i, command = self.command_list[i - 1]))
 
     def choose_command(self):
-        '''Отображение меню и выбор команды '''
         while True:
-            self.show_menu()
-            current_command = int(input('Выберите комманду: ')) - 1
-            if current_command >= len(self.command_list):
+            current_command = input('Выберите комманду: ')
+            try:
+                command_list[current_command]
+            except KeyError: 
                 print('неверная команда')
                 continue
-            if current_command == len(self.command_list) - 1: break
-            if isinstance(self.command_list[current_command], Command):
-                self.command_list[current_command].run_command()
-            if isinstance(self.command_list[current_command], Menu):
-                self.command_list[current_command].show_menu()
-                self.command_list[current_command].choose_command()
 
 
-class Command(object):
-    """Класс используемых комманд в меню"""
-    def __init__(self, name, parent, function = None):
-        self.name = name
-        self.parent = parent
-        self.parent.show_list.append(self.name)
-        self.parent.command_list.append(self)
-        self.function = function
 
-    def run_command(self):
-        """Запуск команды"""
-        print('run_command')
-        self.function()
-
-        
-        
 
 
